b86243865fe1a8d559f28460c424674777efe5eb
Leon Ulcakar <leon.ulcakar@gmail.com>
2022-02-05 19:02:39 +0100

    paddle and game manager

--- pingp/main.cpp
+++ pingp/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
 
 enum eDir {
@@ -7,12 +10,12 @@ enum eDir {
 class cBall
 {
 private:
-	int x;
-	int y;
+	int x{};
+	int y{};
 
-	int ogX;
-	int ogY;
-	eDir direction;
+	int ogX{};
+	int ogY{};
+	eDir direction{};
 
 public:
 	cBall(int posX, int posY) {
@@ -78,22 +81,107 @@ public:
 	}
 	friend ostream& operator<<(ostream& o, cBall c)
 	{
-		o << "Ball [" << c.x << "<" << c.y << "][" << c.direction << "]" << endl;
+		o << "Ball [" << c.x << ", " << c.y << "][" << c.direction << "]" << endl;
 		return o;
 	}
 };
 
+class cPaddle
+{
+private:
+	int x;
+	int y;
+	int ogX{};
+	int ogY{};
+
+public:
+	cPaddle()
+	{
+		x = y = 0;
+	}
+	cPaddle(int posX, int posY) : cPaddle()
+	{
+		ogX = posX;
+		ogY = posY;
+		x = posX;
+		y = posY;
+	}
+	inline void Reset() { x = ogX; y = ogY; }
+	inline int getX() { return x; }
+	inline int getY() { return y; }
+	inline void moveUp() { y--; }
+	inline void moveDown() { y++; }
+
+	friend ostream& operator<<(ostream& o, cPaddle c)
+	{
+		o << "Paddle [" << c.x << ", " << c.y << "]" << endl;
+		return o;
+	}
+
+};
+
+class cGameManager
+{
+private:
+	int width;
+	int height;
+	int score1;
+	int score2;
+	char up1;
+	char up2;
+	char down1;
+	char down2;
+	bool quit;
+	cBall* ball;
+	cPaddle* player1;
+	cPaddle* player2;
+public:
+	cGameManager(int w, int h)
+	{
+		srand(time(NULL));
+		quit = FALSE;
+		up1 = 'w';
+		up2 = 'i';
+		down1 = 's';
+		down2 = 'k';
+		score1 = score2 = 0;
+		width = w;
+		height = h;
+		ball = new cBall(w / 2, h / 2);
+		player1 = new cPaddle(1, h / 2 - 3);
+		player2 = new cPaddle(w-2, h / 2 - 3);
+	}
+	~cGameManager()
+	{
+		delete ball, player1, player2;
+	};
+	void ScoreUp(cPaddle* player)
+	{
+		if (player == player1)
+			score1++;
+		else if (player == player2)
+		{
+			score2++;
+		}
+		ball->Reset();
+		player1->Reset();
+		player2->Reset();
+
+	};
+};
+
 int main() 
 {
-	cBall c(0, 0);
-	std::cout << c << endl;
-	c.randomDirection();
-	std::cout << c << endl;
-	c.Move();
-	std::cout << c << endl;
-	c.randomDirection();
-	c.Move();
-	std::cout << c << endl;
+	cPaddle p1(0, 0);
+	cPaddle p2(10, 0);
+	std::cout << p1 << endl;
+	std::cout << p2 << endl;
+	p1.moveUp();
+	p2.moveDown();
+	std::cout << p1 << endl;
+	std::cout << p2 << endl;
+
+
 
 	return 0;
 }
